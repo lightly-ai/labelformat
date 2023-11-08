06440ccca4c4b8b090a126391f3e79eaee719a9c
Igor Susmelj <igor_7779@hotmail.com>
2023-11-08 11:16:11 +0000

    Add yolov5

---
+++ src/labelformat/formats/yolov5.py
@@ -0,0 +1,17 @@
+from .yolov8 import YOLOv8ObjectDetectionInput, YOLOv8ObjectDetectionOutput
+
+from labelformat.cli.registry import Task, cli_register
+
+"""
+YOLOv5 format follows the same specs as YOLOv8.
+"""
+
+
+@cli_register(format="yolov5", task=Task.OBJECT_DETECTION)
+class YOLOv5ObjectDetectionInput(YOLOv8ObjectDetectionInput):
+    pass
+
+
+@cli_register(format="yolov5", task=Task.OBJECT_DETECTION)
+class YOLOv5ObjectDetectionOutput(YOLOv8ObjectDetectionOutput):
+    pass
