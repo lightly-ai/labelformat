4e3057d6143945e954dde9b2152b0f704ec1dcda
IgorSusmelj <igor_7779@hotmail.com>
2025-01-04 11:36:21 +0100

    Only show analytics when on prod

---
+++ docs/assets/js/analytics.js
@@ -0,0 +1,4 @@
+if (window.location.hostname !== 'labelformat.com') {
+  console.log("Google Analytics disabled on localhost or non-production environments");
+  window['ga-disable-G-K4PH64C9BM'] = true;
+}
