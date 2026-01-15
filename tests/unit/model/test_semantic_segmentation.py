549134ba3fc103e9d73031c342338d3a1af2982e
Michal Szabados <michal@lightly.ai>
2026-01-15 10:19:55 +0100

    Format, better test

--- tests/unit/model/test_semantic_segmentation.py
+++ tests/unit/model/test_semantic_segmentation.py
@@ -7,14 +7,18 @@ from labelformat.model.semantic_segmentation import SemanticSegmentationMask
 
 class TestSemanticSegmentationMask:
     def test_from_array(self) -> None:
-        array = np.array([
-            [1, 1, 2, 2],
-            [1, 1, 2, 2],
-            [3, 3, 3, 3],
-        ], dtype=np.int_)
+        array = np.array(
+            [
+                [1, 1, 2, 2],
+                [2, 1, 1, 1],
+                [3, 3, 3, 3],
+            ],
+            dtype=np.int_,
+        )
         expected_rle = [
-            (1, 2), (2, 2),
-            (1, 2), (2, 2),
+            (1, 2),
+            (2, 3),
+            (1, 3),
             (3, 4),
         ]
         mask = SemanticSegmentationMask.from_array(array=array)
