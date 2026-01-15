9ff83f0a6f3ec2f0fc912c01e6c22faaddf49038
Michal Szabados <michal@lightly.ai>
2026-01-15 14:42:05 +0100

    Convert segmentation mask to binary mask

--- tests/unit/model/test_semantic_segmentation.py
+++ tests/unit/model/test_semantic_segmentation.py
@@ -25,3 +25,35 @@ class TestSemanticSegmentationMask:
         assert mask.category_id_rle == expected_rle
         assert mask.width == 4
         assert mask.height == 3
+
+    def test_to_binary_mask(self) -> None:
+        mask = SemanticSegmentationMask.from_array(
+            array=np.array(
+                [
+                    [1, 1, 2, 2],
+                    [2, 1, 1, 1],
+                    [3, 3, 3, 3],
+                ],
+                dtype=np.int_,
+            )
+        )
+        binary_mask = mask.to_binary_mask(category_id=1)
+        assert binary_mask.get_binary_mask().tolist() == [
+            [1, 1, 0, 0],
+            [0, 1, 1, 1],
+            [0, 0, 0, 0],
+        ]
+
+        binary_mask = mask.to_binary_mask(category_id=2)
+        assert binary_mask.get_binary_mask().tolist() == [
+            [0, 0, 1, 1],
+            [1, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        binary_mask = mask.to_binary_mask(category_id=4)
+        assert binary_mask.get_binary_mask().tolist() == [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
