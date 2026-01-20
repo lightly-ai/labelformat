Fix bounding box computation for segmentations (#62)

tests/unit/model/test_binary_mask_segmentation.py
@@ -6,7 +6,7 @@ from labelformat.model.binary_mask_segmentation import (
     BinaryMaskSegmentation,
     RLEDecoderEncoder,
 )
-from labelformat.model.bounding_box import BoundingBox
+from labelformat.model.bounding_box import BoundingBox, BoundingBoxFormat
 
 
 class TestBinaryMaskSegmentation:
@@ -25,20 +25,23 @@ class TestBinaryMaskSegmentation:
 
     def test_from_rle(self) -> None:
         binary_mask_segmentation = BinaryMaskSegmentation.from_rle(
-            rle_row_wise=[1, 1, 4, 2, 1, 3, 2, 1, 5],
+            rle_row_wise=[6, 1, 4, 2, 1, 3, 2, 1],
             width=5,
             height=4,
             bounding_box=None,
         )
         assert binary_mask_segmentation.width == 5
         assert binary_mask_segmentation.height == 4
-        assert binary_mask_segmentation.bounding_box == BoundingBox(0, 0, 4, 2)
+        assert binary_mask_segmentation.bounding_box == BoundingBox(0, 1, 5, 4)
+        assert binary_mask_segmentation.bounding_box.to_format(
+            format=BoundingBoxFormat.XYWH
+        ) == [0, 1, 5, 3]
         expected: NDArray[np.int_] = np.array(
             [
+                [0, 0, 0, 0, 0],
                 [0, 1, 0, 0, 0],
                 [0, 1, 1, 0, 1],
                 [1, 1, 0, 0, 1],
-                [0, 0, 0, 0, 0],
             ],
             dtype=np.int_,
         )
@@ -142,7 +145,7 @@ def test_compute_bbox_from_rle() -> None:
         width=4,
         height=3,
     )
-    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=3, ymax=2)
+    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=4, ymax=3)
 
     # 0011
     # 0000
@@ -151,7 +154,7 @@ def test_compute_bbox_from_rle() -> None:
         width=4,
         height=2,
     )
-    assert bbox == BoundingBox(xmin=2, ymin=0, xmax=3, ymax=0)
+    assert bbox == BoundingBox(xmin=2, ymin=0, xmax=4, ymax=1)
 
     # 0011
     # 1000
@@ -160,7 +163,7 @@ def test_compute_bbox_from_rle() -> None:
         width=4,
         height=2,
     )
-    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=3, ymax=1)
+    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=4, ymax=2)
 
     # 1111
     bbox = binary_mask_segmentation._compute_bbox_from_rle(
@@ -168,4 +171,4 @@ def test_compute_bbox_from_rle() -> None:
         width=4,
         height=1,
     )
-    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=3, ymax=0)
+    assert bbox == BoundingBox(xmin=0, ymin=0, xmax=4, ymax=1)