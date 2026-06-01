b237b827540f2194e6e43bd4b52a245e08335a71
Lukáš Poláček <lukas@lightly.ai>
2026-06-01 09:43:02 +0200

    Speed up SemanticSegmentationMask
    Use numpy vectorized operations instead of Python for loops.
    
    I've also updated GitHub actions to newer versions (Node 20 will be
    turned off tomorrow).

--- tests/unit/model/test_semantic_segmentation.py
+++ tests/unit/model/test_semantic_segmentation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 
+from labelformat.model.bounding_box import BoundingBox
 from labelformat.model.semantic_segmentation import SemanticSegmentationMask
 
 
@@ -38,6 +39,8 @@ class TestSemanticSegmentationMask:
             )
         )
         binary_mask = mask.to_binary_mask(category_id=1)
+        assert binary_mask.get_rle() == [0, 2, 3, 3, 4]
+        assert binary_mask.bounding_box == BoundingBox(0, 0, 4, 2)
         assert binary_mask.get_binary_mask().tolist() == [
             [1, 1, 0, 0],
             [0, 1, 1, 1],
@@ -45,6 +48,8 @@ class TestSemanticSegmentationMask:
         ]
 
         binary_mask = mask.to_binary_mask(category_id=2)
+        assert binary_mask.get_rle() == [2, 3, 7]
+        assert binary_mask.bounding_box == BoundingBox(0, 0, 4, 2)
         assert binary_mask.get_binary_mask().tolist() == [
             [0, 0, 1, 1],
             [1, 0, 0, 0],
@@ -52,11 +57,27 @@ class TestSemanticSegmentationMask:
         ]
 
         binary_mask = mask.to_binary_mask(category_id=4)
+        assert binary_mask.get_rle() == [12]
+        assert binary_mask.bounding_box == BoundingBox(4, 3, 0, 0)
         assert binary_mask.get_binary_mask().tolist() == [
             [0, 0, 0, 0],
             [0, 0, 0, 0],
             [0, 0, 0, 0],
         ]
+
+    def test_empty_mask(self) -> None:
+        mask = SemanticSegmentationMask.from_array(
+            array=np.empty((0, 3), dtype=np.int_)
+        )
+        assert mask.category_id_rle == []
+        assert mask.width == 3
+        assert mask.height == 0
+        assert mask.category_ids() == set()
+
+        binary_mask = mask.to_binary_mask(category_id=1)
+        assert binary_mask.get_rle() == []
+        assert binary_mask.bounding_box == BoundingBox(3, 0, 0, 0)
+        assert binary_mask.get_binary_mask().shape == (0, 3)
 
     def test_category_ids(self) -> None:
         mask = SemanticSegmentationMask.from_array(
